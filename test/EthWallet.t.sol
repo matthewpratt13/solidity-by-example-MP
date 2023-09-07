a8145d384dcf529220d45905024022a09e854bef
Matthew Pratt <matthew.pratt13@gmail.com>
2023-09-07 18:08:19 +0200

    add EthWallet tests

---
+++ test/EthWallet.t.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+
+import "src/EthWallet.sol";
+
+contract EthWalletTest is Test {
+    EthWallet public wallet;
+
+    address nonOwner = vm.addr(1);
+
+    receive() external payable {}
+
+    function setUp() public {
+        wallet = new EthWallet();
+
+        vm.deal(address(this), 1 ether);
+        vm.deal(address(wallet), 1 ether);
+    }
+
+    function testRevertWhenNonOwnerWithdraws() public {
+        vm.expectRevert("Caller is not the owner");
+
+        vm.prank(nonOwner);
+        wallet.withdraw(0.5 ether);
+    }
+
+    function testWithdraw() public {
+        assertEq(address(this).balance, 1 ether);
+        assertEq(address(wallet).balance, 1 ether);
+
+        wallet.withdraw(0.5 ether);
+
+        assertEq(address(this).balance, 1.5 ether);
+        assertEq(address(wallet).balance, 0.5 ether);
+    }
+
+    function testBalance() public {
+        assertEq(address(wallet).balance, 1 ether);
+        assertEq(address(wallet).balance, wallet.balance());
+    }
+}
