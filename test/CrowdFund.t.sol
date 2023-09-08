8bfeba84341bb12af52910fbd0934e4ce3ea10e9
Matthew Pratt <matthew.pratt13@gmail.com>
2023-09-08 22:45:59 +0200

    update test prep for CrowdFund and Auction

--- test/CrowdFund.t.sol
+++ test/CrowdFund.t.sol
@@ -3,23 +3,14 @@ pragma solidity 0.8.19;
 
 import "forge-std/Test.sol";
 
-import "src/ERC20.sol";
 import "src/CrowdFund.sol";
 
-contract Coin is ERC20 {
-    constructor() ERC20("Coin", "COIN", 18) {}
-
-    function mint(address _to, uint256 _amount) public {
-        _mint(_to, _amount);
-    }
-}
-
 contract CrowdFundTest is Test {
-    Coin public coin;
+    address public coinAddress;
+
     CrowdFund public crowdFund;
 
     function setUp() public {
-        coin = new Coin();
-        crowdFund = new CrowdFund(address(coin));
+        crowdFund = new CrowdFund(coinAddress);
     }
 }
