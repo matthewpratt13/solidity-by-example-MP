c7778d1df64769b7d124bc9bb73042ca5a49d64c
Matthew Pratt <matthew.pratt13@gmail.com>
2023-09-11 16:52:43 +0200

    add onlyOwner

--- src/Marketplace.sol
+++ src/Marketplace.sol
@@ -8,8 +8,8 @@ contract Marketplace is IERC1155TokenReceiver {
         uint256[] tokenIds;
         uint256[] tokenAmounts;
         uint256 tokenPrice;
-        address tokenAddress;
         address seller;
+        address tokenAddress;
     }
 
     IERC1155 public nft;
@@ -21,10 +21,16 @@ contract Marketplace is IERC1155TokenReceiver {
     mapping(uint256 saleId => SaleInfo) public sales;
     mapping(uint256 saleId => mapping(uint256 tokenId => bool soldOut)) public tokenSoldOut;
 
-    event CreateSale(uint256 indexed saleId, address indexed tokenAddress, uint256 tokenPrice);
+    event CreateSale(uint256 indexed saleId, address indexed tokenAddress);
     event CancelSale(uint256 indexed saleId);
-    event TokenSoldOut(uint256 indexed saleId, uint256 indexed tokenId);
     event BuyTokens(uint256 indexed saleId, address indexed buyer, address indexed tokenAddress);
+    event TokenSoldOut(uint256 indexed saleId, uint256 indexed tokenId);
+    event OwnershipTransferred(address indexed user, address indexed newOwner);
+
+    modifier onlyOwner() {
+        require(msg.sender == contractOwner, "Caller is not contract owner");
+        _;
+    }
 
     constructor() {
         contractOwner = msg.sender;
@@ -34,10 +40,9 @@ contract Marketplace is IERC1155TokenReceiver {
         uint256[] calldata _tokenIds,
         uint256[] calldata _tokenAmounts,
         uint256 _tokenPrice,
-        address _tokenAddress,
-        address _seller
-    ) external {
-        require(msg.sender == contractOwner, "Caller is not the owner");
+        address _seller,
+        address _tokenAddress
+    ) external onlyOwner {
         require(_tokenAddress != address(0), "Cannot be zero address");
         require(_tokenIds.length == _tokenAmounts.length, "NFT IDs and amounts do not match");
 
@@ -47,8 +52,8 @@ contract Marketplace is IERC1155TokenReceiver {
             tokenIds: _tokenIds,
             tokenAmounts: _tokenAmounts,
             tokenPrice: _tokenPrice,
-            tokenAddress: _tokenAddress,
-            seller: _seller
+            seller: _seller,
+            tokenAddress: _tokenAddress
         });
 
         unchecked {
@@ -57,18 +62,34 @@ contract Marketplace is IERC1155TokenReceiver {
 
         nft = IERC1155(_tokenAddress);
 
-        nft.safeBatchTransferFrom(msg.sender, address(this), _tokenIds, _tokenAmounts, "");
+        uint256 numberOfTokens = _tokenAmounts.length;
 
-        emit CreateSale(saleId, _tokenAddress, _tokenPrice);
+        for (uint256 i; i < numberOfTokens; ++i) {
+            uint256 tokenBalance = nft.balanceOf(_seller, _tokenIds[i]);
+            require(tokenBalance != 0, "Seller is not a token owner");
+
+            nft.safeTransferFrom(_seller, msg.sender, _tokenIds[i], _tokenAmounts[i], "");
+        }
+
+        emit CreateSale(saleId, _tokenAddress);
     }
 
-    function cancelSale(uint256 _saleId) external {
-        require(msg.sender == contractOwner, "Caller is not the owner");
+    function cancelSale(uint256 _saleId) external onlyOwner {
         require(_saleId < saleIdCounter, "Sale does not exist");
 
         SaleInfo memory sale = sales[_saleId];
 
-        nft.safeBatchTransferFrom(address(this), sale.seller, sale.tokenIds, sale.tokenAmounts, "");
+        uint256 numberOfTokens = sale.tokenAmounts.length;
+
+        uint256 tokenId;
+        uint256 tokenAmount;
+
+        for (uint256 i; i < numberOfTokens;) {
+            tokenId = sale.tokenIds[i];
+            tokenAmount = sale.tokenAmounts[i];
+
+            nft.safeTransferFrom(address(this), sale.seller, tokenId, tokenAmount, "");
+        }
 
         emit CancelSale(_saleId);
     }
@@ -79,13 +100,11 @@ contract Marketplace is IERC1155TokenReceiver {
 
         SaleInfo memory sale = sales[_saleId];
 
-        uint256 tokenAmounts = _tokenAmountsToBuy.length;
-
-        uint256 tokenPrice = sale.tokenPrice;
+        uint256 numberOfTokens = _tokenAmountsToBuy.length;
 
         uint256 totalCost;
 
-        for (uint256 i; i < tokenAmounts; ++i) {
+        for (uint256 i; i < numberOfTokens; ++i) {
             require(sale.tokenAmounts[i] != 0, "No tokens for sale");
             require(!tokenSoldOut[_saleId][_tokenIds[i]], "Token sold out");
 
@@ -95,20 +114,25 @@ contract Marketplace is IERC1155TokenReceiver {
             }
 
             unchecked {
-                totalCost += tokenPrice * _tokenAmountsToBuy[i];
+                totalCost += sale.tokenPrice * _tokenAmountsToBuy[i];
             }
         }
 
         require(msg.sender.balance >= totalCost, "Insufficient balance");
         require(msg.value == totalCost, "Incorrect amount of ETH");
 
-        (bool success,) = sale.seller.call{value: msg.value}("");
-
+        (bool success,) = sale.seller.call{value: totalCost}("");
         require(success, "Transfer failed");
 
         nft.safeBatchTransferFrom(address(this), msg.sender, _tokenIds, _tokenAmountsToBuy, "");
 
         emit BuyTokens(_saleId, msg.sender, sale.tokenAddress);
+    }
+
+    function transferOwnership(address _newOwner) external onlyOwner {
+        contractOwner = _newOwner;
+
+        emit OwnershipTransferred(msg.sender, _newOwner);
     }
 
     function onERC1155Received(address, address, uint256, uint256, bytes calldata) external pure returns (bytes4) {
